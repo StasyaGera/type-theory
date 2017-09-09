da7cc01399104432633705b8d7b6369a186e0d4c
Stanislava Gera <stasya.gera@yandex.ru>
2017-09-09 11:56:51 +0300

    win makefiles

--- hws/make2.bat
+++ hws/make2.bat
@@ -1,3 +1,3 @@
-./make1.sh
+call make1.bat
 
 ocamlc -o hw1_reduction ltype.cmo parser.cmo lexer.cmo hw1.cmo hw1_reduction.mli hw1_reduction.ml
--- hws/make4.bat
+++ hws/make4.bat
@@ -1,4 +1,4 @@
-./make1.sh
-./make3.sh
+call make1.bat
+call make3.bat
 
 ocamlc -o hw2_inference parser.cmo lexer.cmo hw1.cmo hw2_unify.cmo hw2_inference.mli hw2_inference.ml
